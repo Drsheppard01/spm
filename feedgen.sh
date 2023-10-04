f3ece46c8a998fc639fce4cb50ee3a22b29ff14e
DrSheppard <60893791+Drsheppard01@users.noreply.github.com>
2023-10-05 01:40:33 +0300

    update

--- feedgen.sh
+++ feedgen.sh
@@ -2,48 +2,47 @@
 # Title: spm
 # Description: Downloads and installs AppImages and precompiled tar archives.  Can also upgrade and remove installed packages.
 # Dependencies: GNU coreutils, tar, wget, python3.x
-# Author: simonizor
-# Website: http://www.simonizor.gq
+# Author: simonizor, drsheppard
 # License: GPL v2.0 only
 # Generates spm-feed.json containing info for AppImages and precompiled tar packages
 
-rm -f "$HOME"/github/spm/spm-feed.json
-cat >"$HOME"/github/spm/spm-feed.json << EOL
+rm -f "$HOME"/spm/spm-feed.json
+cat >"$HOME"/spm/spm-feed.json << EOL
 {
   "version": 1,
-  "home_page_url": "https://github.com/simoniz0r/spm",
+  "home_page_url": "https://github.com/Drsheppard01/spm",
   "feed_url": "https://simoniz0r.github.io/spm-feed.json",
   "description": "json feed containing package information for spm.",
   "updated_at": "$(date)",
   "expired": false,
   "appimages": [
 EOL
-for image in $(dir -C -w 1 $HOME/github/spm/appimages); do
+for image in $(dir -C -w 1 $HOME/spm/appimages); do
     image="$(echo $image | rev | cut -f2- -d'.' | rev)"
-    INSTALLNAME="$(jq -r '.installname' ~/github/spm/appimages/$image.json)"
-    [ "$INSTALLNAME" = "null" ] && INSTALLNAME="$(jq -r '.name' ~/github/spm/appimages/$image.json | tr '[:upper:]' '[:lower:]')"
-    PACKAGETYPE="$(jq -r '.type' ~/github/spm/appimages/$image.json)"
+    INSTALLNAME="$(jq -r '.installname' ~/spm/appimages/$image.json)"
+    [ "$INSTALLNAME" = "null" ] && INSTALLNAME="$(jq -r '.name' ~/spm/appimages/$image.json | tr '[:upper:]' '[:lower:]')"
+    PACKAGETYPE="$(jq -r '.type' ~/spm/appimages/$image.json)"
     [ "$PACKAGETYPE" = "null" ] && PACKAGETYPE="AppImage"
-    cat >>"$HOME"/github/spm/spm-feed.json << EOL
+    cat >>"$HOME"/spm/spm-feed.json << EOL
     {
-      "name": "$(jq -r '.name' ~/github/spm/appimages/$image.json)",
+      "name": "$(jq -r '.name' ~/spm/appimages/$image.json)",
       "installname": "$INSTALLNAME",
       "type": "$PACKAGETYPE",
-      "description": "$(jq -r '.description' ~/github/spm/appimages/$image.json)",
+      "description": "$(jq -r '.description' ~/spm/appimages/$image.json)",
       "authors": [
         {
-          "name": "$(jq -r '.authors[0].name' ~/github/spm/appimages/$image.json)",
-          "url": "$(jq -r '.authors[0].url' ~/github/spm/appimages/$image.json)"
+          "name": "$(jq -r '.authors[0].name' ~/spm/appimages/$image.json)",
+          "url": "$(jq -r '.authors[0].url' ~/spm/appimages/$image.json)"
         }
       ],
       "links": [
         {
-          "type": "$(jq -r '.links[0].type' ~/github/spm/appimages/$image.json)",
-          "url": "$(jq -r '.links[0].url' ~/github/spm/appimages/$image.json)"
+          "type": "$(jq -r '.links[0].type' ~/spm/appimages/$image.json)",
+          "url": "$(jq -r '.links[0].url' ~/spm/appimages/$image.json)"
         },
         {
-          "type": "$(jq -r '.links[1].type' ~/github/spm/appimages/$image.json)",
-          "url": "$(jq -r '.links[1].url' ~/github/spm/appimages/$image.json)"
+          "type": "$(jq -r '.links[1].type' ~/spm/appimages/$image.json)",
+          "url": "$(jq -r '.links[1].url' ~/spm/appimages/$image.json)"
         }
       ],
       "installinfo": [
@@ -59,7 +58,7 @@ EOL
     echo "$(tput setaf 2)$image has been added to spm-feed.json$(tput sgr0)"
 done
 
-cat >>"$HOME"/github/spm/spm-feed.json << EOL
+cat >>"$HOME"/spm/spm-feed.json << EOL
     {
       "END": "END",
       "name": "END",
